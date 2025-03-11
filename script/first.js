1c7e4c51d4e16c9862ec9a3a95d8fc77819f5c1d
DevAnikRoy <anikroy302@gmail.com>
2025-03-12 00:42:08 +0600

    successfully creat dynamic btn and load Data from API

---
+++ script/first.js
@@ -0,0 +1,27 @@
+console.log('is it working')
+
+
+function loadCategories () {
+    fetch ('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(recive => recive.json())
+    .then(data => displayCategories(data.categories))
+}
+
+function displayCategories (category) {
+    // get the container
+    const categoryContainer = document.getElementById('category-container');
+    // Loop operation on array of object
+    for (let cate of category){
+        // create Element
+        const categoryDiv = document.createElement('div');
+        // set innerHtml of element 
+        categoryDiv.innerHTML = `
+            <button class="bg-[#ececec] text-[#ff1e3c] font-semibold text-sm py-1 px-4 rounded-md hover:cursor-pointer hover:bg-[#ff1e3c] hover:text-white">${cate.category}</button>
+        `;
+        // Append the created element
+        categoryContainer.append(categoryDiv);
+    }
+    
+}
+
+loadCategories()
