cfa7515b33a27355203fa48c14a0d033f22b58d2
DevAnikRoy <anikroy302@gmail.com>
2025-03-12 05:00:12 +0600

    developed successfully vedio section from APi

--- script/videos.js
+++ script/videos.js
@@ -14,15 +14,26 @@ const displayVideos = (videos) => {
         const videoCard = document.createElement("div");
         // set innerHtml
         videoCard.innerHTML =`
-            <div class="card bg-base-100 shadow-sm">
-                <figure>
-                    <img
-                        src="${element.thumbnail}" />
+            <div class="card bg-base-100">
+                <figure class="relative">
+                    <img class="w-full h-[150px] object-cover" src="${element.thumbnail}" />
+                    <span class="absolute bottom-2 right-2 text-sm bg-black px-2 rounded text-white">3hrs 56 min
+                        ago</span>
                 </figure>
-                <div class="card-body">
-                    <h2 class="card-title">${element.title}</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    
+                <div class=" flex gap-3 px-0 py-4">
+                    <div class="profiel">
+                        <div class="avatar">
+                            <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                                <img src="${element.authors[0].profile_picture}" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="intro">
+                        <h2 class="text-sm font-semibold">${element.title}</h2>
+                        <p class="text-sm text-gray-400 flex items-center gap-1">${element.authors[0].profile_name} <img class="w-4 h-4" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
+                        <p class="text-sm text-gray-400">${element.others.views}</p>
+                    </div>
+
                 </div>
             </div>
         `;
